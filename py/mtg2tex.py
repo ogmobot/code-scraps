daaf037e1327d023bd2dedc57564f5e8e36b3169
ogmobot <quantummaestro@gmail.com>
2018-06-11 14:36:40 +1000

    Updates to python and racket programs

--- py/mtg2tex.py
+++ py/mtg2tex.py
@@ -6,21 +6,39 @@ CARDHEIGHT = "82mm"
 
 class MtgCard:
   def __init__(self):
-    name = ""
-    cost = ""
-    cardtype = ""
-    text = ""
-    pt = None
+    self.name = ""
+    self.faces = []
+  def addFace(self, face):
+    face.parent = self
+    self.faces.append(face)
+
+class MtgCardFace:
+  def __init__(self):
+    self.name = "<Placeholder name>"
+    self.cost = None
+    self.typeline = "<Placeholder typeline>"
+    self.text = ""
+    self.pt = None
+    pass
 
 def str2card(cardstring):
-  lines = cardstring.split("\n")
-  result = MtgCard()
-  result.name, result.cost = lines.pop(0).split("\t")
-  result.cardtype = lines.pop(0)
-  if re.match("^[^a-z]*/[^a-z]*$", lines[-1]):
-    result.pt = lines.pop()
-  result.text = "\n".join(lines)
-  return result
+  resultcard = MtgCard()
+  faces = cardstring.split("\n//\n")
+  for face in faces:
+    lines = face.split("\n")
+    result = MtgCardFace()
+    topline = lines.pop(0)
+    if topline.count("\t") == 1:
+      result.name, result.cost = topline.split("\t")
+    else:
+      result.name = topline
+    result.typeline = lines.pop(0)
+    if re.match("^[^a-z]*/[^a-z]*$", lines[-1]):
+      result.pt = lines.pop()
+    result.text = "\n".join(lines)
+    resultcard.addFace(result)
+  resultcard.name = " // ".join([f.name for f in resultcard.faces])
+  return resultcard
 
 def card2tex(card):
   result = r"\fbox{}"
