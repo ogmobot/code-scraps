4487eb13dc49f3bdcd20e98e5eae1eb33dfb0b58
ogmobot <ogmobot@github.com>
2021-01-05 18:08:38 +1100

    Rename modexp.c -> numeric.c

--- c/rsa/numeric.c
+++ c/rsa/numeric.c
@@ -1,13 +1,3 @@
-/*
-#include <gmp.h>
-
-unsigned long modexp(unsigned long b, unsigned int e, unsigned long m) {
-	unsigned long result;
-	mpz_powm(&result, b, (unsigned long int) e, m);
-	return result;
-}
-*/
-
 /* Method of repeated squares */
 unsigned long long modexp(unsigned long long b, unsigned long long e, unsigned long long m) {
 	unsigned long long result = 1;
@@ -18,10 +8,3 @@ unsigned long long modexp(unsigned long long b, unsigned long long e, unsigned l
     }
     return result;
 }
-
-/*
-int main() {
-	printf("%lu\n", modexp(4, 13, 497));
-	return 0;
-}
-*/
